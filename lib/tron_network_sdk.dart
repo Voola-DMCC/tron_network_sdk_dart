55ad92d5621427caa7bf574bad4845425821497c
Oleg <robbbbb748@gmail.com>
2022-12-13 20:07:10 +0200

    trongrid export

--- lib/tron_network_sdk.dart
+++ lib/tron_network_sdk.dart
@@ -5,5 +5,6 @@ library tron_network_sdk;
 
 export 'src/wallet/wallet.dart';
 export 'src/http_api/http_api.dart';
+export 'src/trongrid_api/trongrid_api.dart';
 export 'src/utils.dart';
 // TODO: Export any libraries intended for clients of this package.
